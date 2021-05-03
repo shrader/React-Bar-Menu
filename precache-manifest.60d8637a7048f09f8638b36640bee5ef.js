7d02f8fb957523091b600d7dc716871b6c186026
John <figurefactory@gmail.com>
2021-05-03 00:11:28 -0400

    Updates

---
+++ precache-manifest.60d8637a7048f09f8638b36640bee5ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f4d9b2f2ed3a847f810fbb10634031",
+    "url": "/React-Bar-Menu/index.html"
+  },
+  {
+    "revision": "4722e55f81e39407ac04",
+    "url": "/React-Bar-Menu/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "bcd64195da9175f254b3",
+    "url": "/React-Bar-Menu/static/css/main.8581c446.chunk.css"
+  },
+  {
+    "revision": "4722e55f81e39407ac04",
+    "url": "/React-Bar-Menu/static/js/2.34024063.chunk.js"
+  },
+  {
+    "revision": "e317015e6aea0983d75cc5b07d2258fa",
+    "url": "/React-Bar-Menu/static/js/2.34024063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd64195da9175f254b3",
+    "url": "/React-Bar-Menu/static/js/main.4bf0ed96.chunk.js"
+  },
+  {
+    "revision": "fb6f39e09e426cad3c89",
+    "url": "/React-Bar-Menu/static/js/runtime-main.51a663ab.js"
+  }
+]);
